694bafd95cf41add08cfec4aa1cee47690b857a5
AlianeIsimbi <isimbialiane@gmail.com>
2025-12-08 11:36:05 +0200

    Initial project commit

---
+++ src/test/java/BaseTests/BaseTest.java
@@ -0,0 +1,28 @@
+package BaseTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+public class BaseTest {
+
+    private WebDriver driver;
+    protected HomePage homePage;
+@BeforeMethod
+    public void setUp(){
+        System.setProperty("web.chrome.drive", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+    }
+
+@AfterMethod
+    public void close(){
+        driver.close();
+    }
+
+
+
+}
